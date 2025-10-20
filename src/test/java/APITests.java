d626e7996a2f2565ad7fbda2a72c3c8213301d72
dhruv481 <dguptadg01@gmail.com>
2025-10-21 02:27:48 +0530

    fixed https issue

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -5,17 +5,20 @@ import org.testng.annotations.Test;
 public class APITests {
 
     @Test
-    void test1(){
-        Response response = RestAssured.get("http://reqres.in/api/users?page=2");
+    void test1() {
+        Response response = RestAssured.given().header("User-Agent", "Java RestAssured").get("https://reqres.in/api/users?page=2");
 
         System.out.println("Response:" + response.asString());
-        System.out.println("Status Code : "+ response.getStatusCode());
-        System.out.println("Body : "+ response.getBody());
-        System.out.println("Header :"+ response.getHeader("content-type"));
+        System.out.println("Status Code : " + response.getStatusCode());
+        System.out.println("Body : " + response.getBody());
+        System.out.println("Header :" + response.getHeader("content-type"));
     }
 
     @Test
     void test2() {
-        RestAssured.get("http://reqres.in/api/users?page=2").then().statusCode(200);
+        RestAssured.useRelaxedHTTPSValidation();
+        RestAssured.given()
+                .header("User-Agent", "Java RestAssured")
+                .get("https://reqres.in/api/users?page=2").then().statusCode(200);
     }
 }
